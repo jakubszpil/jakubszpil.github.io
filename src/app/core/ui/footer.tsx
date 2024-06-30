19f151e880a4e6ed25e9ef6867ec6c5becde4144
Jakub Szpil <kuba0122@gmail.com>
2024-06-30 11:53:35 +0200

    * fix footer links

--- src/app/core/ui/footer.tsx
+++ src/app/core/ui/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer(props: FooterProps) {
           &copy; {new Date().getFullYear()} jakubszpil
         </p>
 
-        <nav className="flex items-center gap-1 flex-wrap -ml-3">
+        <nav className="flex items-center flex-wrap -ml-3">
           {props.children}
         </nav>
       </div>
