b69df8214883c1d90f8780c32698253d137722d5
jakubszpil <46812496+jakubszpil@users.noreply.github.com>
2024-07-05 13:18:16 +0000

    Deploying to gh-pages from @ jakubszpil/jakubszpil.github.io@8e1f005a7d411994d7a8239e2ec3be1a09f997bb 🚀

--- assets/routing-BIdQrWQK.js
+++ assets/routing-BIdQrWQK.js
@@ -1,4 +1,4 @@
-import{r as v,t as zr,R as Hr}from"./vendor-xdSkcd26.js";/**
+import{r as v,t as zr,R as Hr}from"./vendor-BG4p725_.js";/**
  * @remix-run/router v1.16.1
  *
  * Copyright (c) Remix Software Inc.
