0c1e229dae8f6785d0f0ac399e1f69878e66c6b5
Jakub Szpil <kuba0122@gmail.com>
2024-07-07 20:44:05 +0200

    * support PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 
 import { chunks, mdx, minify } from "./vite.plugins";
 
@@ -17,6 +18,41 @@ export default defineConfig(() => {
       react(),
       tsconfigPaths(),
       minify(["html", "svg", "json"]),
+      pwa({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "Jakub Szpil",
+          short_name: "jakubszpil",
+          description: "Jakub Szpil - Blog, portfolio i kursy frontendowe",
+          theme_color: "#ffffff",
+          lang: "pl",
+          display: "standalone",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
     ],
     css: {
       postcss: {
