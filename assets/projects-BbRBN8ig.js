4f57f805cfa989b793bc3e527aed468437c0bce7
jakubszpil <46812496+jakubszpil@users.noreply.github.com>
2024-07-11 13:53:25 +0000

    Deploying to gh-pages from @ jakubszpil/jakubszpil.github.io@95ca750986590636e9c7f1390aee5999a728ff52 🚀

---
+++ assets/projects-BbRBN8ig.js
@@ -0,0 +1,1 @@
+import{c as e,U as t,a as o}from"./shared-BXppTPjZ.js";import"./vendor-DyiBHA70.js";const r=[e("").addModule(async()=>({default:t}))],[a,n,g,i]=o(Object.assign({}),"technologies");export{a as g,r};
