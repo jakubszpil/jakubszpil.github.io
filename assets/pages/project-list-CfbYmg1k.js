f1ddd1ec71d8baf39b8977afca91f58d74aa03c5
jakubszpil <46812496+jakubszpil@users.noreply.github.com>
2024-10-20 12:45:26 +0000

    Deploying to gh-pages from @ jakubszpil/jakubszpil.github.io@a6699b7354cfec09137512c1a74779357f886faa 🚀

---
+++ assets/pages/project-list-CfbYmg1k.js
@@ -0,0 +1,1 @@
+import{j as t}from"../react-vvBGimMH.js";import{S as o,U as r}from"../components-DRS2fUYE.js";import"../utils-DaD1UTAx.js";import"../routing-CpIhnanH.js";import"../runtime-COQdTiDd.js";function p(){return t.jsxs(t.Fragment,{children:[t.jsx(o,{title:"Portfolio"}),t.jsx(r,{})]})}export{p as default};
