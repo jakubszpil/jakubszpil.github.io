fc4a5e0c4e9c5d5ecdba2142e5ec26c448c80457
Jakub Szpil <kuba0122@gmail.com>
2024-12-20 20:30:55 +0100

    * cleanup

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,6 @@
 import type { RouteObject } from "react-router";
 
-import {
-  type ExtractPathsFromRoutes,
-  index,
-  prefix,
-  route,
-} from "./lib/routing";
+import { index, prefix, route } from "./lib/routing";
 import { routes as articlesRoutes } from "./modules/articles/articles.routes";
 import { routes as coursesRoutes } from "./modules/courses/courses.routes";
 import { routes as projectsRoutes } from "./modules/projects/projects.routes";
@@ -20,5 +15,3 @@ export const routes = [
   route("me", () => import("./routes/about")),
   route("*", () => import("./routes/not-found")),
 ] as const satisfies RouteObject[];
-
-export type Path = ExtractPathsFromRoutes<typeof routes>;
