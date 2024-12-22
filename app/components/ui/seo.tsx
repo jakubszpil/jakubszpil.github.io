954b3fbe61d12d7dcb58798310f6fb7a033933cf
Jakub Szpil <kuba0122@gmail.com>
2024-12-22 15:19:55 +0100

    * fix seo meta tags

--- app/components/ui/seo.tsx
+++ app/components/ui/seo.tsx
@@ -1,3 +1,5 @@
+import { useLocation } from "react-router";
+
 import { config } from "~/lib/config";
 
 export interface SeoMeta {
@@ -14,19 +16,32 @@ export interface SeoProps {
 }
 
 export const Seo = (props: SeoProps) => {
+  const { pathname } = useLocation();
+
   const title = props.title ?? config.meta.defaultTitle;
   const description = props.description ?? config.meta.description;
   const keywords = props.keywords?.join(",");
+
+  const origin = import.meta.env.PROD
+    ? "https://jakubszpil.github.io"
+    : "http://localhost:5173";
+
+  const url = `${origin}${pathname}`;
 
   return (
     <>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:url" content={url} />
     </>
   );
 };
