405d2b6dea36238d9d6131706dd9c501d68dedeb
Jakub Szpil <kuba0122@gmail.com>
2025-01-03 16:44:12 +0100

    * fix script

--- script.mjs
+++ script.mjs
@@ -1,5 +1,4 @@
-import { existsSync } from "node:fs";
-import { readdir, rename, rmdir } from "node:fs/promises";
+import { readdir, rename } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { fileURLToPath, URL } from "node:url";
 
@@ -16,14 +15,7 @@ try {
   for (const file of files) {
     const path = file.replace("\\index", "");
     await rename(file, path);
-    const dir = path.replace(".html", "");
-
-    if (existsSync(dir)) {
-      const files = await readdir(dir);
-      if (!files.length) {
-        await rmdir(dir);
-      }
-    }
+    console.log(path);
   }
 } catch (error) {
   console.log(error);
