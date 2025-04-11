62b4bfde6f6728cac5c4986ebfe5a952bd33783a
Jakub Szpil <kuba0122@gmail.com>
2025-04-11 13:01:37 +0200

    feature - fix flash screen on theme change

---
+++ public/theme.js
@@ -0,0 +1,36 @@
+document.documentElement.classList.add("switching-theme");
+
+const theme = localStorage.getItem("theme");
+
+switch (theme) {
+  case "DARK": {
+    document.documentElement.classList.add("dark");
+    break;
+  }
+
+  case "LIGHT": {
+    document.documentElement.classList.remove("dark");
+    break;
+  }
+
+  case "SYSTEM":
+  case null: {
+    const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (matches) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    break;
+  }
+
+  default: {
+    localStorage.removeItem("theme");
+  }
+}
+
+setTimeout(() => {
+  document.documentElement.classList.remove("switching-theme");
+}, 100);
