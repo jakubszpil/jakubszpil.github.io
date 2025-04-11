c7f484794a5e5a792cab248b79819aaf4c1fe62d
Jakub Szpil <46812496+jakubszpil@users.noreply.github.com>
2025-04-11 13:17:13 +0200

    feature - fix flash screen on theme change (#42)

--- app/root.tsx
+++ app/root.tsx
@@ -3,10 +3,13 @@ import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 import "./styles.css";
 
-declare module "react" {
-  interface ScriptHTMLAttributes<T> extends HTMLAttributes<T> {
-    fetchPriority?: HTMLScriptElement["fetchPriority"];
-  }
+function injectScript(script: string) {
+  const result = script
+    .trim()
+    .replace(/  +/g, "")
+    .replace(/(\r\n|\n|\r)/gm, "");
+
+  return result;
 }
 
 export function Layout({ children }: { children: ReactNode }) {
@@ -23,6 +26,44 @@ export function Layout({ children }: { children: ReactNode }) {
           type="font/ttf"
           crossOrigin="anonymous"
         />
+        <script>
+          {injectScript(`
+            const theme = localStorage.getItem("theme");
+
+            switch (theme) {
+              case "DARK": {
+                document.documentElement.classList.add("dark");
+                break;
+              }
+
+              case "LIGHT": {
+                document.documentElement.classList.remove("dark");
+                break;
+              }
+
+              case "SYSTEM":
+              case null: {
+                const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
+
+                if (matches) {
+                  document.documentElement.classList.add("dark");
+                } else {
+                  document.documentElement.classList.remove("dark");
+                }
+                  
+                break;
+              }
+
+              default: {
+                localStorage.removeItem("theme");
+              }
+            }
+          `)}
+        </script>
+        <script>
+          {injectScript(`globalThis.timestamp = ${import.meta.env.TIMESTAMP}`)}
+        </script>
+        <script type="module" src="/fetch.js" />
         <Meta />
         <Links />
       </head>
---
+++ public/fetch.js
@@ -0,0 +1,93 @@
+const _fetch = globalThis.fetch;
+const _createElement = globalThis.document.createElement;
+const _timestamp = globalThis.timestamp;
+const _storedTimestamp = localStorage.getItem("timestamp");
+
+if (Number(_timestamp) !== Number(_storedTimestamp)) {
+  if (_storedTimestamp) {
+    if (await window.caches.has(_storedTimestamp)) {
+      await window.caches.delete(_storedTimestamp);
+    }
+  }
+
+  localStorage.setItem("timestamp", _timestamp);
+}
+
+const cache = await window.caches.open(_timestamp);
+
+function createRequestUrl(requestInput) {
+  if (requestInput instanceof URL) {
+    return requestInput;
+  }
+
+  if (requestInput instanceof Request) {
+    return new URL(requestInput.url);
+  }
+
+  return new URL(requestInput);
+}
+
+function createRequestInit(requestInit) {
+  /** @type {RequestInit} */
+  const init = requestInit ?? {};
+  init.cache = "force-cache";
+  return init;
+}
+
+function createRequest(requestInput, requestInit) {
+  const url = createRequestUrl(requestInput);
+  const init = createRequestInit(requestInit);
+
+  url.search = "";
+  url.searchParams.set("timestamp", _timestamp);
+
+  return new Request(url, init);
+}
+
+globalThis.fetch = async (input, init) => {
+  const requestUrl = createRequestUrl(input);
+
+  if (requestUrl.origin !== location.origin) {
+    return _fetch(input, init);
+  }
+
+  const request = createRequest(input, init);
+
+  const cached = await cache.match(request);
+
+  if (cached) {
+    return cached.clone();
+  }
+
+  const response = await _fetch(request);
+
+  await cache.put(request, response.clone());
+
+  return response.clone();
+};
+
+globalThis.document.createElement = (tag, options) => {
+  const element = _createElement.bind(document)(tag, options);
+
+  if (element instanceof HTMLLinkElement) {
+    const setAttribute = element.setAttribute;
+
+    element.setAttribute = async (key, value) => {
+      if (element.rel === "prefetch" && key === "href") {
+        const pathname = `${value}?timestamp=${_timestamp}`;
+
+        const url = new URL(`${location.origin}${pathname}`);
+
+        const matched = await cache.match(new Request(url));
+
+        if (!matched) setAttribute.call(element, key, pathname);
+        return;
+      }
+
+      setAttribute.call(element, key, value);
+      return;
+    };
+  }
+
+  return element;
+};
