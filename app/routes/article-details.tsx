b7006b752849d49bfa12eaa756089eadbf2462ac
Jakub Szpil <kuba0122@gmail.com>
2025-04-11 15:40:05 +0200

    feature - await loader data

--- app/routes/article-details.tsx
+++ app/routes/article-details.tsx
@@ -1,4 +1,4 @@
-import { Await, useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { useLoaderData, type LoaderFunctionArgs } from "react-router";
 
 import Categories from "@/components/blog/categories";
 import { EditResource } from "@/components/ui/edit-resource";
@@ -6,37 +6,33 @@ import { Seo } from "@/components/ui/seo";
 import { getArticle } from "@/lib/articles";
 
 export async function loader({ params: { slug } }: LoaderFunctionArgs) {
-  const article = getArticle(slug!);
+  const article = await getArticle(slug!);
 
-  return { article };
+  return article;
 }
 
 export default function ArticleDetails() {
-  const { article } = useLoaderData<typeof loader>();
+  const article = useLoaderData<typeof loader>();
 
   return (
-    <Await resolve={article}>
-      {(article) => (
-        <>
-          <Seo
-            title={article.title}
-            description={article.description}
-            keywords={article.keywords}
-          />
+    <>
+      <Seo
+        title={article.title}
+        description={article.description}
+        keywords={article.keywords}
+      />
 
-          <header className="prose container">
-            <h1>{article.title}</h1>
-            <Categories categories={article.categories} />
-          </header>
+      <header className="prose container">
+        <h1>{article.title}</h1>
+        <Categories categories={article.categories} />
+      </header>
 
-          <article
-            className="prose container"
-            dangerouslySetInnerHTML={{ __html: article.content }}
-          />
+      <article
+        className="prose container"
+        dangerouslySetInnerHTML={{ __html: article.content }}
+      />
 
-          <EditResource resourceUrl={article.resourceUrl} />
-        </>
-      )}
-    </Await>
+      <EditResource resourceUrl={article.resourceUrl} />
+    </>
   );
 }
