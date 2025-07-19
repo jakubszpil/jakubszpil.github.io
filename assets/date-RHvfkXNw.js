cbded3278f96521acabbd2e89aac8c450fd56d5c
jakubszpil <46812496+jakubszpil@users.noreply.github.com>
2025-07-19 14:28:46 +0000

    Deploying to gh-pages from @ jakubszpil/jakubszpil.github.io@7ee1c23d64cf7a21bbbb13f26bffbb5979754568 🚀

--- assets/date-RHvfkXNw.js
+++
@@ -1,1 +0,0 @@
-function i(e){return new Intl.DateTimeFormat("pl-PL",{day:"numeric",month:"long",year:"numeric"}).format(e)}function o(){return new Date().getFullYear()}function f(e){const n=new Date;if(isNaN(e.getTime()))throw new Error("Invalid startDate date format");const t=(n.getTime()-e.getTime())/(1e3*60*60*24)/365;return Math.round(t*2)/2}export{i as a,f as b,o as g};
---
+++ assets/search-CCQsz1Qz.js
@@ -0,0 +1,1 @@
+import{c as t}from"./search-Bc8rD4Dc.js";import"./chunk-EF7DTUVF-D-tBg9jn.js";export{t as clientLoader};
