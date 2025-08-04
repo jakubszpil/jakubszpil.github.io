cfd4b3ae0aa5af879dcac01e221fd2a5e724dca8
Jakub Szpil <kuba0122@gmail.com>
2025-08-04 18:35:26 +0200

    * small fix - fix initial prefetching

--- app/hooks/use-initial-load-prefetch.ts
+++ app/hooks/use-initial-load-prefetch.ts
@@ -1,19 +1,29 @@
-import { useMatches } from "react-router";
+import { useMatches, UNSAFE_FrameworkContext } from "react-router";
 
 import { usePrefetch } from "./use-prefetch";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 export function useInitialLoadPrefetch() {
   const [mounted, setMounted] = useState(false);
   const matches = useMatches();
+  const context = use(UNSAFE_FrameworkContext);
+
+  const routes = context?.manifest.routes ?? {};
 
   const match = matches.at(-1);
+
+  const route = match ? routes[match.id] : undefined;
+
   const resource = mounted
     ? undefined
     : match
-    ? match.pathname === "/"
-      ? "/_root.data"
-      : `${match.pathname}.data`
+    ? route
+      ? route.hasLoader
+        ? match.pathname === "/"
+          ? "/_root.data"
+          : `${match.pathname}.data`
+        : undefined
+      : undefined
     : undefined;
 
   usePrefetch(resource);
