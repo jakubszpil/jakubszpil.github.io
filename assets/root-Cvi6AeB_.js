bbf3072367fe9bea131e42111e7cdaedc7c0a4a3
jakubszpil <46812496+jakubszpil@users.noreply.github.com>
2025-10-19 16:39:24 +0000

    Deploying to gh-pages from @ jakubszpil/jakubszpil.github.io@441003334195529e7e851d714ac8a10d1de9d918 🚀

---
+++ assets/root-Cvi6AeB_.js
@@ -0,0 +1,14 @@
+import{w as a,M as c,L as o,S as n,p as i,O as p}from"./chunk-OIYGIGL5-Rdhby1S0.js";import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as m}from"./index-DJQXGD61.js";const t="/assets/styles-CguqvXB_.css";function d(s){return s.trim().replace(/\s{2,}/g," ").replace(/(\r\n|\n|\r)/g,"").replace(/\s*=\s*/g,"=").replace(/\s*\{\s*/g,"{").replace(/\s*\}\s*/g,"}").replace(/\s*\|\|\s*/g,"||").replace(/\s*\?\s*/g,"?").replace(/\s*:\s*/g,":").replace(/\s*;\s*/g,";").replace(/\s*===\s*/g,"===").replace(/\)\s*/g,")").replace(/\s*\(/g,"(")}function r(s){return e.jsx("script",{dangerouslySetInnerHTML:{__html:d(s.code)}})}function f(s){return m.preloadModule(s.src,s.options),e.jsx("script",{type:"module",src:s.src})}function u({children:s}){return e.jsxs("html",{lang:"pl",children:[e.jsxs("head",{children:[e.jsx("meta",{charSet:"utf-8"}),e.jsx("meta",{name:"viewport",content:"width=device-width, initial-scale=1"}),e.jsx("link",{rel:"icon",href:"/favicon.svg",sizes:"any",type:"image/svg+xml"}),e.jsx("link",{rel:"preload",as:"style",href:t}),e.jsx("link",{rel:"stylesheet",href:t}),e.jsx("link",{rel:"preload prefetch",href:"/fonts/geist.ttf",as:"font",type:"font/ttf",crossOrigin:"anonymous"}),e.jsx(r,{code:`globalThis.timestamp = ${1760891955675}`}),e.jsx(r,{code:`
+            let k = "theme";
+            let t = "dark";
+            let s = localStorage;
+            let c = document.documentElement.classList;
+            let v = s.getItem(k);
+            if (v === null || v === "SYSTEM") 
+              matchMedia("(prefers-color-scheme: dark)").matches
+                ? c.add(t)
+                : c.remove(t);
+            else if (v === "DARK") c.add(t);
+            else if (v === "LIGHT") c.remove(t);
+            else s.removeItem(k);
+          `}),e.jsx(f,{src:"/fetch.js"}),e.jsx(c,{}),e.jsx(o,{})]}),e.jsxs("body",{style:{WebkitTapHighlightColor:"transparent"},children:[s,e.jsx(n,{getKey:l=>Object.values(l).filter(Boolean).join(".")}),e.jsx(i,{})]})]})}const x=a(function(){return e.jsx(p,{})});export{u as Layout,x as default};
