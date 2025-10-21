92159643eef4de700effb158caf942cb2d17951b
Jakub Szpil <kuba0122@gmail.com>
2025-10-21 22:50:21 +0200

    * update

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -49,7 +49,7 @@ export default function Projects(props: ProjectsProps) {
                 variant="link"
                 asChild
                 size="sm"
-                className="underline p-0"
+                className="underline p-0! h-auto"
               >
                 <a
                   data-testid="link"
@@ -57,7 +57,7 @@ export default function Projects(props: ProjectsProps) {
                   target="_blank"
                 >
                   <IconBrandGithub className="w-5 h-5 mr-2" />
-                  Otwórz GitHub
+                  GitHub
                 </a>
               </Button>
             </div>
