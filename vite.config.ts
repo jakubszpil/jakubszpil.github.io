cfaf83a95dff7949bbb9c7afaf8243dde9e605bd
Jakub Szpil <kuba0122@gmail.com>
2025-11-22 10:04:41 +0100

    * small update - switch from jsdom to happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" />
+
 import { defineConfig, type UserConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -26,22 +27,11 @@ export default defineConfig((): UserConfig => {
       }),
       tsconfigPaths(),
     ],
-    build: {
-      minify: true,
-      rolldownOptions: {
-        treeshake: true,
-        optimization: {
-          inlineConst: {
-            mode: "smart",
-          },
-        },
-      },
-    },
     test: {
       include: ["app/**/*.{test,spec}.{ts,tsx}"],
       globals: true,
       watch: false,
-      environment: "jsdom",
+      environment: "happy-dom",
       passWithNoTests: true,
       pool: "threads",
     },
