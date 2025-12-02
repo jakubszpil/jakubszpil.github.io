cb851b01ae7468dca9b48336efd1bd195aa40803
Jakub Szpil <kuba0122@gmail.com>
2025-12-02 13:43:20 +0100

    * update

--- app/routes.ts
+++ app/routes.ts
@@ -1,15 +1,15 @@
 import { type RouteConfig, prefix, route } from "@react-router/dev/routes";
 
-import articleRoutes from "./modules/articles/routes";
-import courseRoutes from "./modules/courses/routes";
-import projectRoutes from "./modules/projects/routes";
-import coreRoutes from "./core/routes";
+import setupRoutesForArticles from "./modules/articles/routes";
+import setupRoutesForCourses from "./modules/courses/routes";
+import setupRoutesForProjects from "./modules/projects/routes";
+import setupRoutesForCore from "./core/routes";
 
 export default [
   route("", "core/feature/layout.tsx", [
-    ...prefix("blog", articleRoutes()),
-    ...prefix("learning", courseRoutes()),
-    ...prefix("portfolio", projectRoutes()),
-    ...prefix("", coreRoutes()),
+    ...prefix("blog", setupRoutesForArticles()),
+    ...prefix("learning", setupRoutesForCourses()),
+    ...prefix("portfolio", setupRoutesForProjects()),
+    ...prefix("", setupRoutesForCore()),
   ]),
 ] satisfies RouteConfig;
