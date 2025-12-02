0e62548c8d2a4aa90a60aa46bdfb06168d7197be
Jakub Szpil <kuba0122@gmail.com>
2025-12-02 17:18:07 +0100

    * move content to individual packages

--- packages/feature-courses/src/lib/data-access/course-service.ts
+++ packages/feature-courses/src/lib/data-access/course-service.ts
@@ -3,7 +3,7 @@ import { ResourceService } from "@packages/shared/server";
 import type { Course } from "./course";
 
 export class CourseService extends ResourceService<Course>(
-  import.meta.glob<string>("../../../../../content/courses/*.md", {
+  import.meta.glob<string>("../../../content/*.md", {
     import: "default",
     query: "?raw",
     eager: true,
--- packages/feature-projects/src/lib/data-access/project-service.ts
+++ packages/feature-projects/src/lib/data-access/project-service.ts
@@ -3,7 +3,7 @@ import { ResourceService } from "@packages/shared/server";
 import type { Project } from "./project";
 
 export class ProjectService extends ResourceService<Project>(
-  import.meta.glob<string>("../../../../../content/projects/*.md", {
+  import.meta.glob<string>("../../../content/*.md", {
     import: "default",
     query: "?raw",
     eager: true,
