0ca918e1df13fe921421079123f67d77679c1df1
Jakub Szpil <kuba0122@gmail.com>
2025-12-13 17:03:28 +0100

    * fix

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,6 @@ export default defineConfig((): UserConfig => {
       environment: "happy-dom",
       passWithNoTests: true,
       pool: "threads",
-      isolate: false,
       reporters: [["default", { summary: false }]],
     },
   };
