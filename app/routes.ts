ecac8f5e276389739469acfb6dada156c7b90135
Jakub Szpil <kuba0122@gmail.com>
2026-01-22 21:42:51 +0100

    * simplify routes config

--- app/routes.ts
+++ app/routes.ts
@@ -1,36 +1,29 @@
 import {
   type RouteConfig,
   layout,
-  prefix,
   route,
   index,
 } from "@react-router/dev/routes";
 
 export default [
   layout("routes/layout.tsx", [
-    ...prefix("blog", [
-      index("routes/article-list.tsx", { id: "article-list" }),
-      route("kategorie/:category", "routes/article-list.tsx", {
-        id: "article-list-with-category",
-      }),
-      route(":slug", "routes/article-detail.tsx", { id: "article-detail" }),
-    ]),
-    ...prefix("learning", [
-      index("routes/course-list.tsx", { id: "course-list" }),
-      route("kategorie/:category", "routes/course-list.tsx", {
-        id: "course-list-with-category",
-      }),
-      route(":slug", "routes/course-detail.tsx", { id: "course-detail" }),
-    ]),
-    ...prefix("portfolio", [
-      index("routes/project-list.tsx", { id: "project-list" }),
-      route("technologie/:technology", "routes/project-list.tsx", {
-        id: "project-list-with-technology",
-      }),
-    ]),
-    index("routes/home.tsx", { id: "home" }),
-    route("search", "routes/search.tsx", { id: "search" }),
-    route("me", "routes/about.tsx", { id: "about" }),
-    route("*", "routes/not-found.tsx", { id: "not-found" }),
+    route("blog", "routes/article-list.tsx"),
+    route("blog/kategorie/:category", "routes/article-list.tsx", {
+      id: "article-list-with-category",
+    }),
+    route("blog/:slug", "routes/article-detail.tsx"),
+    route("learning", "routes/course-list.tsx"),
+    route("learning/kategorie/:category", "routes/course-list.tsx", {
+      id: "course-list-with-category",
+    }),
+    route("learning/:slug", "routes/course-detail.tsx"),
+    route("portfolio", "routes/project-list.tsx"),
+    route("portfolio/technologie/:technology", "routes/project-list.tsx", {
+      id: "project-list-with-technology",
+    }),
+    index("routes/home.tsx"),
+    route("search", "routes/search.tsx"),
+    route("me", "routes/about.tsx"),
+    route("*", "routes/not-found.tsx"),
   ]),
 ] satisfies RouteConfig;
