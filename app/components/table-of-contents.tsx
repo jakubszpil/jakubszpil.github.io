f0622dedb0d63ac00ba773a02c505297dd6db049
Jakub Szpil <kuba0122@gmail.com>
2026-02-22 12:11:35 +0100

    feature - change margin in toc

--- app/components/table-of-contents.tsx
+++ app/components/table-of-contents.tsx
@@ -51,7 +51,7 @@ export function TableOfContents({
   return (
     <>
       <div className="container py-0 grid grid-cols-1 lg:grid-cols-3 lg:gap-x-8">
-        <div className="container hidden lg:block px-0 text-sm! lg:order-2 sticky! max-h-[calc(100dvh-(var(--spacing))*33)] h-full top-16 bg-background z-40 rounded-tr-lg">
+        <div className="container hidden lg:block px-0 text-sm! lg:order-2 lg:pt-0 sticky! max-h-[calc(100dvh-(var(--spacing))*37)] h-full top-19 bg-background z-40 rounded-tr-lg">
           <ScrollArea className="h-full relative">
             <TableOfContentsList className="relative h-max">
               <TableOfContentsTitle className="bg-background sticky top-0 text-base left-0 pb-3 mb-0">
@@ -75,7 +75,9 @@ export function TableOfContents({
           </ScrollArea>
         </div>
 
-        <div className="container px-0 lg:col-span-2">{children(ref)}</div>
+        <div className="container px-0 lg:col-span-2 lg:pt-0">
+          {children(ref)}
+        </div>
       </div>
     </>
   );
