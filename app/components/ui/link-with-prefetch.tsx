8a9a5ad30dd2012a1a6fac81b5a4c71d3815ee76
Jakub Szpil <kuba0122@gmail.com>
2026-02-26 21:32:38 +0100

    * small update - use reload link

--- app/components/ui/link-with-prefetch.tsx
+++ app/components/ui/link-with-prefetch.tsx
@@ -3,5 +3,5 @@ import { Link, type LinkProps } from "react-router";
 export interface LinkWithPrefetchProps extends LinkProps {}
 
 export function LinkWithPrefetch(props: LinkWithPrefetchProps) {
-  return <Link {...props} prefetch="viewport" />;
+  return <Link {...props} reloadDocument prefetch="viewport" />;
 }
