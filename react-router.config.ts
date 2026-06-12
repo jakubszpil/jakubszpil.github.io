8e530b87fad789f1f5d3ef427e9ce0fba30e0c55
Jakub Szpil <kuba0122@gmail.com>
2026-06-12 16:43:01 +0200

    * update

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  routeDiscovery: {
+    mode: "initial",
+  },
   future: {
     v8_viteEnvironmentApi: true,
     v8_splitRouteModules: true,
