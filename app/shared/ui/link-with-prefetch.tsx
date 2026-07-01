1ff1323e7022af28ecfec5289b77e4ca272014e4
Jakub Szpil <kuba0122@gmail.com>
2026-07-01 20:52:56 +0200

    feature - use prediction for prefetching

--- app/shared/ui/link-with-prefetch.tsx
+++ app/shared/ui/link-with-prefetch.tsx
@@ -1,13 +1,59 @@
+import {
+  useForesight,
+  type ForesightRegisterOptionsWithoutElement,
+} from "@foresightjs/react";
+import { useCallback, useMemo, useState } from "react";
 import { Link, type LinkProps } from "react-router";
 
 import { useIsMobile } from "../data-access/use-mobile";
 
-export interface LinkWithPrefetchProps extends LinkProps {}
+export interface LinkWithPrefetchProps
+  extends LinkProps, Omit<ForesightRegisterOptionsWithoutElement, "callback"> {
+  ref?: (node: HTMLAnchorElement | null) => void;
+}
 
-export function LinkWithPrefetch(props: LinkWithPrefetchProps) {
+export function LinkWithPrefetch({
+  hitSlop,
+  name,
+  meta,
+  reactivateAfter,
+  enabled,
+  ref,
+  ...props
+}: LinkWithPrefetchProps) {
+  const [shouldPrefetch, setShouldPrefetch] = useState(false);
+
+  const { elementRef } = useForesight<HTMLAnchorElement>({
+    callback: () => {
+      console.log(props.to);
+      return setShouldPrefetch(true);
+    },
+    hitSlop,
+    name,
+    meta,
+    reactivateAfter,
+    enabled,
+  });
+
+  const assignRefs = useCallback(
+    (el: HTMLAnchorElement | null) => {
+      elementRef(el);
+      ref?.(el);
+    },
+    [elementRef, ref],
+  );
+
   const isMobile = useIsMobile();
 
-  const prefetch = props.prefetch ?? (isMobile ? "viewport" : "intent");
+  const prefetch = useMemo(
+    () =>
+      props.prefetch === "none"
+        ? "none"
+        : shouldPrefetch
+          ? "viewport"
+          : (props.prefetch ?? (isMobile ? "viewport" : "intent")),
+    [props.prefetch, isMobile, shouldPrefetch],
+  );
 
-  return <Link {...props} prefetch={prefetch} />;
+  return <Link {...props} ref={assignRefs} prefetch={prefetch} />;
 }
